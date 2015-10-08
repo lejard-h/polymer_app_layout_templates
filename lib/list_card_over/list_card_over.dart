8cbf3370a471a4172f3be3ac0e458e0c82f4c1f0
Hadrien Lejard <hadrien.lejard@gmail.com>
2015-10-08 21:06:31 +0200

    list_card_over

--- lib/list_card_over/list_card_over.dart
+++ lib/list_card_over/list_card_over.dart
@@ -8,7 +8,49 @@ library polymer_app_layout.layout_list_card_over;
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 
-@PolymerRegister('layout-list_card_over')
+import 'package:polymer_elements/iron_icons.dart';
+import 'package:polymer_elements/iron_media_query.dart';
+import 'package:polymer_elements/paper_material.dart';
+import 'package:polymer_elements/paper_drawer_panel.dart';
+import 'package:polymer_elements/paper_header_panel.dart';
+import 'package:polymer_elements/paper_icon_button.dart';
+import 'package:polymer_elements/paper_toolbar.dart';
+import 'package:polymer_elements/paper_item.dart';
+import 'package:polymer_elements/paper_menu.dart';
+
+@PolymerRegister('layout-list-card-over')
 class LayoutListCardOver extends PolymerElement {
+
+    bool isMobile;
+    String mainMode;
+    String drawerWidth;
+    String toolbarClass;
+
     LayoutListCardOver.created() : super.created();
+
+    PaperDrawerPanel get drawerPanel => $['drawerPanel'];
+
+    _updateAttributes() {
+        set('isMobile', isMobile);
+        set('mainMode', mainMode);
+        set('drawerWidth', drawerWidth);
+        set('toolbarClass', toolbarClass);
+    }
+
+    ready() {
+        _updateAttributes();
+    }
+
+    listTap(event, _) {
+       drawerPanel.closeDrawer();
+    }
+
+    @Observe('isMobile')
+    isMobileChanged(isMobile) {
+        mainMode = isMobile ? 'seamed' : 'cover';
+        drawerWidth = isMobile ? '100%' : '320px';
+        toolbarClass = isMobile ? '' : 'tall';
+        _updateAttributes();
+        updateStyles();
+    }
 }
--- web/index.dart
+++ web/index.dart
@@ -7,6 +7,7 @@ import 'package:polymer/polymer.dart';
 import 'package:polymer_app_layout_template/nav_view/nav_view.dart';
 import 'package:polymer_app_layout_template/nav_list_detail/nav_list_detail.dart';
 import 'package:polymer_app_layout_template/nav_cards/nav_cards.dart';
+import 'package:polymer_app_layout_template/list_card_over/list_card_over.dart';
 
 main() async {
   await initPolymer();
