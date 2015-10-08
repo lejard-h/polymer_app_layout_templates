9d6bef65fe0271fc65124af88bba8a72625c6a47
Hadrien Lejard <hadrien.lejard@gmail.com>
2015-10-08 20:22:49 +0200

    nav-view

--- lib/nav_view/nav_view.dart
+++ lib/nav_view/nav_view.dart
@@ -8,7 +8,13 @@ library polymer_app_layout.layout_nav_view;
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 
+import 'package:polymer_elements/iron_icons.dart';
+import 'package:polymer_elements/paper_drawer_panel.dart';
+import 'package:polymer_elements/paper_header_panel.dart';
+import 'package:polymer_elements/paper_icon_button.dart';
+import 'package:polymer_elements/paper_toolbar.dart';
+
 @PolymerRegister('layout-nav-view')
 class LayoutNavView extends PolymerElement {
-    LayoutNavView.created() : super.created();
+  LayoutNavView.created() : super.created();
 }
