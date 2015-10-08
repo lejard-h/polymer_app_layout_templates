e49620f2c658c32f313500e8e66fb52097167e16
Hadrien Lejard <hadrien.lejard@gmail.com>
2015-10-08 20:53:04 +0200

    nav-cards

--- lib/nav_cards/nav_cards.dart
+++ lib/nav_cards/nav_cards.dart
@@ -8,7 +8,14 @@ library polymer_app_layout.layout_nav_cards;
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 
-@PolymerRegister('layout-nav_cards')
+import 'package:polymer_elements/iron_icons.dart';
+import 'package:polymer_elements/paper_drawer_panel.dart';
+import 'package:polymer_elements/paper_header_panel.dart';
+import 'package:polymer_elements/paper_icon_button.dart';
+import 'package:polymer_elements/paper_toolbar.dart';
+import 'package:polymer_elements/paper_material.dart';
+
+@PolymerRegister('layout-nav-cards')
 class LayoutNavCards extends PolymerElement {
     LayoutNavCards.created() : super.created();
 }
