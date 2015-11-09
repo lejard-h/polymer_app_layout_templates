a054d05ac6614c0387b6435bd0772282e561b5d8
Hadrien Lejard <hadrien.lejard@gmail.com>
2015-11-09 18:07:46 +0100

    models doc

--- lib/models/app_page.dart
+++ lib/models/app_page.dart
@@ -6,11 +6,16 @@ import "package:polymer/polymer.dart";
 import "page.dart";
 
 class AppPage extends Page {
+  /// define polymer material icon for the page
   @reflectable
   dynamic icon;
 
+  /// link to the sub page
   @reflectable
   AppPage child;
+
+  String toString() =>
+      "{ name: $name, path: $path, element: $element, isDefault: $isDefault, menu: $menu, hideLeftNav: $hideLeftNav, icon: $icon}";
 
   AppPage(name, path, element, {isDefault: false, menu: true, hideLeftNav: false, icon, child: null})
       : super(name, path, element, isDefault: isDefault, menu: menu, hideLeftNav: hideLeftNav, child: child) {
@@ -21,7 +26,4 @@ class AppPage extends Page {
     }
     this.child = child;
   }
-
-  String toString() =>
-      "{ name: $name, path: $path, element: $element, isDefault: $isDefault, menu: $menu, hideLeftNav: $hideLeftNav, icon: $icon}";
 }
--- lib/models/page.dart
+++ lib/models/page.dart
@@ -6,33 +6,41 @@ import "package:polymer/polymer.dart";
 import 'package:route_hierarchical/client.dart';
 
 class Page extends JsProxy {
+
+  /// path of the page
   @reflectable
   final String path;
 
+  /// name of the page
   @reflectable
   final String name;
 
+  /// element of the page
+  /// Can be an [HtmlElement] or the element name as a [String]
   @reflectable
   final dynamic element;
 
+  /// definne if page is default, home
   @reflectable
   final bool isDefault;
 
+  /// define if page is visible in the menu
   @reflectable
   final bool menu;
 
+  /// define if the left menu is hide for this page
   @reflectable
   final hideLeftNav;
 
+  /// link to the sub page
   @reflectable
   Page child;
 
   String toString() =>
       "{ name: $name, path: $path, element: $element, isDefault: $isDefault, menu: $menu, hideLeftNav: $hideLeftNav}";
 
-  Page(this.name, this.path, this.element,
-      {this.child: null, this.isDefault: false, this.menu: true, this.hideLeftNav: false});
-
+  /// When enter in a new route.
+  /// This function call the enterRoute function of the current element if it's possible.
   @reflectable
   enterRoute(RouteEvent e) {
     if (element != null) {
@@ -41,4 +49,7 @@ class Page extends JsProxy {
       } catch (e) {}
     }
   }
+
+  Page(this.name, this.path, this.element,
+      {this.child: null, this.isDefault: false, this.menu: true, this.hideLeftNav: false});
 }
